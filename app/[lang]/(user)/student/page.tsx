39e2d2bc0432883de1cad54358816a0a9cb12950
Mubarek Ahmed Mohammed <mubarekahmed349@gmail.com>
2025-10-29 17:49:51 +0300

    Update package dependencies and refactor Telegram integration in student page. Added @telegram-apps/sdk for Telegram data retrieval, modified user ID handling, and improved loading state display.

--- app/[lang]/(user)/student/page.tsx
+++ app/[lang]/(user)/student/page.tsx
@@ -1,27 +1,33 @@
 'use client';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+import { retrieveRawInitData } from '@telegram-apps/sdk'
+
 
 export default function Page() {
-  const [userId, setUserId] = useState<number | null>(null);
+const initDataRaw = retrieveRawInitData()
+console.log(initDataRaw)
+//   const [userId, setUserId] = useState<number | null>(null);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.Telegram?.WebApp?.initDataUnsafe) {
-      const tgUser = window.Telegram.WebApp.initDataUnsafe.user;
-      if (tgUser?.id) {
-        setUserId(tgUser.id);
-      }
-    }
-  }, []);
+//   useEffect(() => {
+//         // Check if the window object exists and if Telegram.WebApp.initDataUnsafe is available
+//         if (typeof window !== 'undefined' && window.Telegram?.WebApp?.initDataUnsafe) {
+//           // Get the user information from the Telegram WebApp initData
+//           const tgUser = window.Telegram.WebApp.initDataUnsafe.user;
+//           if (tgUser?.id) {
+//             setUserId(tgUser.id);    // Set the user ID in the state
+//           }
+//         }
+//     }, []);
 
   return (
     <div>
       <h1>Telegram Mini App</h1>
-      {userId ? (
-        <p>Chat ID (User ID): {userId}</p>
+      
+      {initDataRaw ? (
+        <p>Chat ID (User ID): {initDataRaw}</p>
       ) : (
         <p>Loading user info...</p>
       )}
     </div>
   );
 }
-
